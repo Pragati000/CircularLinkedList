36fc427c4f4cdf57e768ebea28f59c9ea142cd3e
Pragati000 <pragati.goswami_ccv172gla.ac.in>
2019-03-12 21:57:12 -0700

    adding constructor
    Node(public,data(int)=data,next(Node)=next)

--- src/main/CircularLinkedList.java
+++ src/main/CircularLinkedList.java
@@ -12,6 +12,11 @@ public class CircularLinkedList {
            this.data = data;
        }
 
+       public Node(int data, Node next) {
+           this.data = data;
+           this.next = next;
+       }
+
 
    }
 }
